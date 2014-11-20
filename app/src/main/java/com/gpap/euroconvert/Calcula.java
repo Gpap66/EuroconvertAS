f98d9ad28027be78b5c5d42bd8868a05809fb4b7
Gpap66 <devandmore@gmail.com>
2014-11-20 10:39:50 -0500

    Initial commit

---
+++ app/src/main/java/com/gpap/euroconvert/Calcula.java
@@ -0,0 +1,18 @@
+package com.gpap.euroconvert;
+
+public class Calcula {
+    double apot = 0;
+
+    public Double toEuroKg(double dol, double pd) {
+
+        double seKg = 0.453 * pd;
+        double seEr = 0.76 * dol;
+
+        apot = seEr / seKg;
+        apot = Math.round(apot * 100);
+        apot = apot / 100;
+        return apot;
+
+    }
+
+}
